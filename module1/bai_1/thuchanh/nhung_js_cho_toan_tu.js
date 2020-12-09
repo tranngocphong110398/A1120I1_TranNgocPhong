ba37ee071b3040e25f555f130aa7099d71b2ca6b
phongtran <tranngocphong110398@gmail.com>
2020-12-09 13:24:19 +0700

    upload thuc hanh su dung toan tu

---
+++ module1/bai_1/thuchanh/nhung_js_cho_toan_tu.js
@@ -0,0 +1,9 @@
+// document.write("Nhúng file js vào file toán tử")
+let inputwidth;
+let inputheight;
+inputwidth=prompt("nhập chiều rộng : ");
+inputheight=prompt("nhập chiều dài : ");
+width=parseInt(inputwidth);
+height=parseInt(inputheight);
+let area=width*height;
+document.writeln("Diện tích hình chử nhật là : " + area);
