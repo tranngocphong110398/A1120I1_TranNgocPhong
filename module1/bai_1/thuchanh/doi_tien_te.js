3abfa73dcea6a74bb08ae19053fabd62bbbd8db2
phongtran <tranngocphong110398@gmail.com>
2020-12-09 19:51:37 +0700

    upload  chuyen doi tien te

---
+++ module1/bai_1/thuchanh/doi_tien_te.js
@@ -0,0 +1,26 @@
+function check() {
+var a,b,c,doi;
+a=parseInt(document.getElementById("amount").value);
+b=parseInt(document.getElementById("test1").value);
+c=parseInt(document.getElementById("test2").value);
+alert("b = " +b);
+alert("c = " +c);
+
+        if (b===0&&c===0){
+            doi=a/23000;
+        }
+        if (b==0&&c==1){
+            doi=a*1;
+        }
+        if (b==1&&c==0){
+            doi=a*1;
+        }
+        if (b==1&&c==1){
+            doi=a*23000;
+        }
+
+        alert(doi);
+
+    document.getElementById("in").value = doi;
+
+}
